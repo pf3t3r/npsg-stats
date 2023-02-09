eadb46f07c8fdf0cc3892f5a340997ce9cc294c3
pf3t3r <90827321+pf3t3r@users.noreply.github.com>
2023-02-09 02:33:53 +0100

    Histogram update; testKS improval; preallocation
    - change limits on moments figure
    - correct log histograms
    - use newer histogram function
    - remove deprecated method to find NaN/zero values in testKS.m
    - preallocated variables to save time

--- testKS.m
+++ testKS.m
@@ -1,109 +1,72 @@
+%% Preamble
 clear; clc; close all;
 addpath("baroneRoutines\");
 
 % Set Figure Parameters
-% set(groot,'defaultAxesXGrid','on');
-% set(groot,'defaultAxesYGrid','on');
-% set(groot, 'defaultFigureUnits', 'centimeters', 'defaultFigurePosition', [3 5 28 12]);
+set(groot,'defaultAxesXGrid','on');
+set(groot,'defaultAxesYGrid','on');
+set(groot, 'defaultFigureUnits', 'centimeters', 'defaultFigurePosition', [3 5 15 15]);
 set(0,'defaultAxesFontSize',10);
 
-%% Load Test Data
+%% Load Data
 chloro = load("datafiles\chloro.mat"','chloro256').chloro256;
 chloroL = load("datafiles\chloro.mat"','chloro256_lang').chloro256_lang;
 time = load("datafiles\chloro.mat","time256").time256;
 
-%% Remove NaN or Zeros
+%% Calculate the Kolmogorov-Smirnov Statistic for Chloropigment
 
-tickyboi = [];
-antiTick = [];
-tickyboiL = [];
-antiTickL = [];
-
-for i = 1:329
-    for j = 1:129
-        if (~any(chloro(j,i)))
-            tickyboi = [tickyboi i];
-            break;
-        else
-            antiTick = [antiTick i];
-            break;
-        end
-    end
-end
-
-
-for i = 1:329
-    for j = 1:129
-        if (~any(chloroL(j,i)))
-            tickyboiL = [tickyboiL i];
-            break;
-        else
-            antiTickL = [antiTickL i];
-            break;
-        end
-    end
-end
-
-%% Create Arrays of chl-a and time which have no zero or NaN data
-chloroNew = chloro(:,[antiTick]);
-timeNew = time(:,[antiTick]);
-chloroNewL = chloro(:,[antiTickL]);
-timeNewL = time(:,[antiTickL]);
-
-%% Look
-
-
-% MLEpArray = [];
-% for i=1:depthMeasurements
-%     disp(i);
-%     MLEp = statsplot2(chloroNew(i,:));
-%     MLEpArray = [MLEpArray MLEp];
-% end
-
-%%
 depthMeasurements = 129;
-clc;
+
+mleE = zeros(5,2,depthMeasurements);
+ksE = zeros(5,depthMeasurements);
+nllE = zeros(5,depthMeasurements);
+
+mleL = zeros(5,2,depthMeasurements);
+ksL = zeros(5,depthMeasurements);
+nllL = zeros(5,depthMeasurements);
+
 % Eulerian
-for i=1:depthMeasurements
+for i = 1:depthMeasurements
     disp(i);
     tmp = chloro(i,:);
     tmp(isnan(tmp) | tmp<=0) = [];
-    [MLEp(:,:,i),KSp(:,i),nll(:,i)] = statsplot2(tmp,'noplot');
+    [mleE(:,:,i),ksE(:,i),nllE(:,i)] = statsplot2(tmp,'noplot');
 end
 clear tmp;
 
 % Lagrangian
-for i=3:depthMeasurements
+for i = 1:depthMeasurements
     disp(i);
     tmp = chloroL(i,:);
     tmp(isnan(tmp) | tmp<=0) = [];
-    [~,KSpL(:,i),nllL(:,i)] = statsplot2(tmp,'noplot');
+    [mleL(:,:,i),ksL(:,i),nllL(:,i)] = statsplot2(tmp,'noplot');
 end
 
-%%
-set(groot, 'defaultFigureUnits', 'centimeters', 'defaultFigurePosition', [3 5 15 15]);
+%% Plot the KS Statistic vs Depth for Five Distributions
 
 ax1 = figure;
+% Eulerian
 subplot(1,2,1)
-plot(KSp(1,:),linspace(0,-2*depthMeasurements,depthMeasurements),'Color',[0 0 0],'DisplayName','Normal');
+plot(ksE(1,:),linspace(0,-2*depthMeasurements,depthMeasurements),'Color',[0 0 0],'DisplayName','Normal');
 hold on
-plot(KSp(2,:),linspace(0,-2*depthMeasurements,depthMeasurements),'Color',[0 0 0],'LineStyle','--','DisplayName','Lognormal');
-plot(KSp(3,:),linspace(0,-2*depthMeasurements,depthMeasurements),'Color','red','DisplayName','Weibull');
-plot(KSp(4,:),linspace(0,-2*depthMeasurements,depthMeasurements),'Color','red','LineStyle','--','DisplayName','Gamma');
-plot(KSp(5,:),linspace(0,-2*depthMeasurements,depthMeasurements),'Color','red','LineStyle',':','DisplayName','Exp');
+plot(ksE(2,:),linspace(0,-2*depthMeasurements,depthMeasurements),'Color',[0 0 0],'LineStyle','--','DisplayName','Lognormal');
+plot(ksE(3,:),linspace(0,-2*depthMeasurements,depthMeasurements),'Color','red','DisplayName','Weibull');
+plot(ksE(4,:),linspace(0,-2*depthMeasurements,depthMeasurements),'Color','red','LineStyle','--','DisplayName','Gamma');
+plot(ksE(5,:),linspace(0,-2*depthMeasurements,depthMeasurements),'Color','red','LineStyle',':','DisplayName','Exp');
 hold off
 legend();
 xlabel('p-value');
 ylabel('Depth [m]');
 title('Eulerian KS-Test');
 
+% Lagrangian
 subplot(1,2,2)
-plot(KSpL(1,:),linspace(128,-128,depthMeasurements),'Color',[0 0 0],'DisplayName','Normal');
+plot(ksL(1,:),linspace(128,-128,depthMeasurements),'Color',[0 0 0],'DisplayName','Normal');
 hold on
-plot(KSpL(2,:),linspace(128,-128,depthMeasurements),'Color',[0 0 0],'LineStyle','--','DisplayName','Lognormal');
-plot(KSpL(3,:),linspace(128,-128,depthMeasurements),'Color','red','DisplayName','Weibull');
-plot(KSpL(4,:),linspace(128,-128,depthMeasurements),'Color','red','LineStyle','--','DisplayName','Gamma');
-plot(KSpL(5,:),linspace(128,-128,depthMeasurements),'Color','red','LineStyle',':','DisplayName','Exp');
+plot(ksL(2,:),linspace(128,-128,depthMeasurements),'Color',[0 0 0],'LineStyle','--','DisplayName','Lognormal');
+plot(ksL(3,:),linspace(128,-128,depthMeasurements),'Color','red','DisplayName','Weibull');
+plot(ksL(4,:),linspace(128,-128,depthMeasurements),'Color','red','LineStyle','--','DisplayName','Gamma');
+plot(ksL(5,:),linspace(128,-128,depthMeasurements),'Color','red','LineStyle',':','DisplayName','Exp');
 hold off
 legend();
 xlabel('p-value');
