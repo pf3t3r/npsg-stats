38b50a67f0134881e5d70ea3f562c67a12309aa4
pf3t3r <90827321+pf3t3r@users.noreply.github.com>
2023-10-24 22:51:45 +0200

    Sensitivity Test: extreme sample sizes
    For a range of (typical) means and a set standard deviation, test the sensitivity of K-S / Lil / A-D lognormal tests to lognormal random data with extremely low (4, 8, 12, 16, 20, 24), and extremely high (500, 1000, 1500, 2000, 2500, 3000) sample sizes.

--- testPrec2.m
+++ testPrec2.m
@@ -12,28 +12,31 @@ sd = 0.4;
 
 %% Initialise random arrays
 
+% Adapt number initialised
+a1 = 500; a2 = 1000; a3 = 1500; a4 = 2000; a5 = 2500; a6 = 3000;
+
 tmp20 = []; tmp40 = []; tmp60 = []; tmp80 = []; tmp100 = []; tmp1000 = [];
 
-tmp20 = nan(20,50,6);
-tmp40 = nan(40,50,6);
-tmp60 = nan(60,50,6);
-tmp80 = nan(80,50,6);
-tmp100 = nan(100,50,6);
-tmp1000 = nan(1000,50,6);
+tmp20 = nan(a1,50,6);
+tmp40 = nan(a2,50,6);
+tmp60 = nan(a3,50,6);
+tmp80 = nan(a4,50,6);
+tmp100 = nan(a5,50,6);
+tmp1000 = nan(a6,50,6);
 
 for j = 1:50
 for i = 1:length(mu)
-    t20 = lognrnd(mu(i),sd,20,1);
+    t20 = lognrnd(mu(i),sd,a1,1);
     tmp20(:,j,i) = t20;
-    t40 = lognrnd(mu(i),sd,40,1);
+    t40 = lognrnd(mu(i),sd,a2,1);
     tmp40(:,j,i) = t40;
-    t60 = lognrnd(mu(i),sd,60,1);
+    t60 = lognrnd(mu(i),sd,a3,1);
     tmp60(:,j,i) = t60;
-    t80 = lognrnd(mu(i),sd,80,1);
+    t80 = lognrnd(mu(i),sd,a4,1);
     tmp80(:,j,i) = t80;
-    t100 = lognrnd(mu(i),sd,100,1);
+    t100 = lognrnd(mu(i),sd,a5,1);
     tmp100(:,j,i) = t100;
-    t1000 = lognrnd(mu(i),sd,1000,1);
+    t1000 = lognrnd(mu(i),sd,a6,1);
     tmp1000(:,j,i) = t1000;
 end
 end 
@@ -55,23 +58,23 @@ end
 %% K-S Lognormal Test of Lognormal Data
 
 ax = figure;
-plot(mu,mean(ksL20),'Color','#a6cee3','DisplayName','n = 20');
+plot(mu,mean(ksL20),'Color','#a6cee3','DisplayName',sprintf('n = %d', a1));
 hold on
 plot(mu,mean(ksL20)+std(ksL20),'Color','#a6cee3','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(ksL20)-std(ksL20),'Color','#a6cee3','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(ksL40),'Color','#1f78b4','DisplayName','n = 40');
+plot(mu,mean(ksL40),'Color','#1f78b4','DisplayName',sprintf('n = %d', a2));
 plot(mu,mean(ksL40)+std(ksL40),'Color','#1f78b4','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(ksL40)-std(ksL40),'Color','#1f78b4','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(ksL60),'Color','#b2df8a','DisplayName','n = 60');
+plot(mu,mean(ksL60),'Color','#b2df8a','DisplayName',sprintf('n = %d', a3));
 plot(mu,mean(ksL60)+std(ksL60),'Color','#b2df8a','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(ksL60)-std(ksL60),'Color','#b2df8a','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(ksL80),'Color','#33a02c','DisplayName','n = 80');
+plot(mu,mean(ksL80),'Color','#33a02c','DisplayName',sprintf('n = %d', a4));
 plot(mu,mean(ksL80)+std(ksL80),'Color','#33a02c','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(ksL80)-std(ksL80),'Color','#33a02c','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(ksL100),'Color','#fb9a99','DisplayName','n = 100');
+plot(mu,mean(ksL100),'Color','#fb9a99','DisplayName',sprintf('n = %d', a5));
 plot(mu,mean(ksL100)+std(ksL100),'Color','#fb9a99','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(ksL100)-std(ksL100),'Color','#fb9a99','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(ksL1000),'Color','#e31a1c','DisplayName','n = 1000');
+plot(mu,mean(ksL1000),'Color','#e31a1c','DisplayName',sprintf('n = %d', a6));
 plot(mu,mean(ksL1000)+std(ksL1000),'Color','#e31a1c','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(ksL1000)-std(ksL1000),'Color','#e31a1c','LineStyle',':',HandleVisibility="off");
 hold off
@@ -79,62 +82,62 @@ ylabel('p-value');
 xlabel('mean');
 legend(Location="best");
 title('K-S Lognormal Test of Random Lognormal Data','Sensitivity Analysis (\sigma = 0.4)');
-exportgraphics(ax,'figures/prec/runKs.png'); clear ax;
+exportgraphics(ax,'figures/prec/runKs_xHigh.png'); clear ax;
 
 %% Lilliefors Lognormal Test of Lognormal Data
 % It appears as though the p-value is not returned for values over 0.5. Not
 % sure why yet.
 
 ax2 = figure;
-plot(mu,mean(lilL20),'Color','#a6cee3','DisplayName','n = 20');
+plot(mu,mean(lilL20),'Color','#a6cee3','DisplayName',sprintf('n = %d', a1));
 hold on
 plot(mu,mean(lilL20)+std(lilL20),'Color','#a6cee3','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(lilL20)-std(lilL20),'Color','#a6cee3','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(lilL40),'Color','#1f78b4','DisplayName','n = 40');
+plot(mu,mean(lilL40),'Color','#1f78b4','DisplayName',sprintf('n = %d', a2));
 plot(mu,mean(lilL40)+std(lilL40),'Color','#1f78b4','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(lilL40)-std(lilL40),'Color','#1f78b4','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(lilL60),'Color','#b2df8a','DisplayName','n = 60');
+plot(mu,mean(lilL60),'Color','#b2df8a','DisplayName',sprintf('n = %d', a3));
 plot(mu,mean(lilL60)+std(lilL60),'Color','#b2df8a','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(lilL60)-std(lilL60),'Color','#b2df8a','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(lilL80),'Color','#33a02c','DisplayName','n = 80');
+plot(mu,mean(lilL80),'Color','#33a02c','DisplayName',sprintf('n = %d', a4));
 plot(mu,mean(lilL80)+std(lilL80),'Color','#33a02c','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(lilL80)-std(lilL80),'Color','#33a02c','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(lilL100),'Color','#fb9a99','DisplayName','n = 100');
+plot(mu,mean(lilL100),'Color','#fb9a99','DisplayName',sprintf('n = %d', a5));
 plot(mu,mean(lilL100)+std(lilL100),'Color','#fb9a99','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(lilL100)-std(lilL100),'Color','#fb9a99','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(lilL1000),'Color','#e31a1c','DisplayName','n = 1000');
+plot(mu,mean(lilL1000),'Color','#e31a1c','DisplayName',sprintf('n = %d', a6));
 plot(mu,mean(lilL1000)+std(lilL1000),'Color','#e31a1c','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(lilL1000)-std(lilL1000),'Color','#e31a1c','LineStyle',':',HandleVisibility="off");
 ylabel('p-value');
 xlabel('mean');
 legend(Location="best");
 title('Lilliefors Lognormal Test of Random Lognormal Data','Sensitivity Analysis (\sigma = 0.4)');
-exportgraphics(ax2,'figures/prec/runLil.png'); clear ax2;
+exportgraphics(ax2,'figures/prec/runLil_xHigh.png'); clear ax2;
 
 %% A-D Lognormal Test of Lognormal Data
 
 ax3 = figure;
-plot(mu,mean(adL20),'Color','#a6cee3','DisplayName','n = 20');
+plot(mu,mean(adL20),'Color','#a6cee3','DisplayName',sprintf('n = %d', a1));
 hold on
 plot(mu,mean(adL20)+std(adL20),'Color','#a6cee3','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(adL20)-std(adL20),'Color','#a6cee3','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(adL40),'Color','#1f78b4','DisplayName','n = 40');
+plot(mu,mean(adL40),'Color','#1f78b4','DisplayName',sprintf('n = %d', a2));
 plot(mu,mean(adL40)+std(adL40),'Color','#1f78b4','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(adL40)-std(adL40),'Color','#1f78b4','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(adL60),'Color','#b2df8a','DisplayName','n = 60');
+plot(mu,mean(adL60),'Color','#b2df8a','DisplayName',sprintf('n = %d', a3));
 plot(mu,mean(adL60)+std(adL60),'Color','#b2df8a','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(adL60)-std(adL60),'Color','#b2df8a','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(adL80),'Color','#33a02c','DisplayName','n = 80');
+plot(mu,mean(adL80),'Color','#33a02c','DisplayName',sprintf('n = %d', a4));
 plot(mu,mean(adL80)+std(adL80),'Color','#33a02c','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(adL80)-std(adL80),'Color','#33a02c','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(adL100),'Color','#fb9a99','DisplayName','n = 100');
+plot(mu,mean(adL100),'Color','#fb9a99','DisplayName',sprintf('n = %d', a5));
 plot(mu,mean(adL100)+std(adL100),'Color','#fb9a99','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(adL100)-std(adL100),'Color','#fb9a99','LineStyle',':',HandleVisibility="off");
-plot(mu,mean(adL1000),'Color','#e31a1c','DisplayName','n = 1000');
+plot(mu,mean(adL1000),'Color','#e31a1c','DisplayName',sprintf('n = %d', a6));
 plot(mu,mean(adL1000)+std(adL1000),'Color','#e31a1c','LineStyle',':',HandleVisibility="off");
 plot(mu,mean(adL1000)-std(adL1000),'Color','#e31a1c','LineStyle',':',HandleVisibility="off");
 ylabel('p-value');
 xlabel('mean');
 legend(Location="best");
 title('A-D Lognormal Test of Random Lognormal Data','Sensitivity Analysis (\sigma = 0.4)');
-exportgraphics(ax3,'figures/prec/runAd.png'); clear ax3;
+exportgraphics(ax3,'figures/prec/runAd_xHigh.png'); clear ax3;
